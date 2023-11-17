6aa6e3aa8a4c5565ddd92b7352744402dc5a5a3c
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-17 16:37:19 +0200

    squash!

--- app/components/TodoData.js
+++ app/components/TodoData.js
@@ -1,12 +1,9 @@
-
 class TodoData {
-
-    constructor(text, id) {
-        this.text = text;
-        this.timestamp = new Date().toLocaleDateString();
-        this.id = id;
-    }
-
+	constructor(text, id) {
+		this.text = text;
+		this.timestamp = new Date().toLocaleDateString();
+		this.id = id;
+	}
 }
 
 export default TodoData;
